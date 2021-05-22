09b53d07cef44000e5e01542f36246a255dd2a41
Sangmin In <me@sangmin.in>
2021-05-23 06:34:03 +0900

    manage secret key

--- junction_app/db/__init__.py
+++ junction_app/db/__init__.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from junction_app import DB_URL
 
-engine = create_engine("")
+
+engine = create_engine(DB_URL)
 Base = declarative_base(bind=engine)
 session = sessionmaker(bind=engine)
 
